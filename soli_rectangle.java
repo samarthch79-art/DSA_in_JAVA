3cf80581e6f54d7b9a98b437def3f0cabcb63a32
samarthch79-art <samarthch79@gmail.com>
2026-04-16 18:43:04 +0530

    solid rectangle printing program
    Implement a program to print a solid rectangle of asterisks.

---
+++ soli_rectangle.java
@@ -0,0 +1,13 @@
+//printing a solid rectangle
+
+
+class Main{
+    public static void main(String []args){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<5;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }    
+    }
+}
