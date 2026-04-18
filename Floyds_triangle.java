61dced71bce85e25884b7984e798da8ca0c4bbce
samarthch79-art <samarthch79@gmail.com>
2026-04-19 02:10:41 +0530

    Floyd's triangle pattern printing
    Implement Floyd's triangle pattern printing in Java.

---
+++ Floyds_triangle.java
@@ -0,0 +1,15 @@
+// printing floids triangle pattern
+
+
+class Main{
+    public static void main(String []args){
+        int n=1;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(n+" ");
+                n++;
+            }
+            System.out.println();
+        }
+    }
+}
