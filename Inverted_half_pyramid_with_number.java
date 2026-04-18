f5a236cc744c5fa2269de5132712b497cededed7
samarthch79-art <samarthch79@gmail.com>
2026-04-18 18:49:33 +0530

    Add inverted half pyramid pattern with numbers
    Implement inverted half pyramid pattern using numbers.

---
+++ Inverted_half_pyramid_with_number.java
@@ -0,0 +1,13 @@
+// printing inverted half pyramid with numbers
+
+
+class Pattern {
+    public static void main(String[] args) {
+        for (int i = 10; i >0; i--) {
+            for (int j = 1; j <i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
