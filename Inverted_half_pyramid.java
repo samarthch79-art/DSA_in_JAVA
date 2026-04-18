9e293ee13d01f7f44aa99fd954e460a7e849011d
samarthch79-art <samarthch79@gmail.com>
2026-04-18 18:38:00 +0530

    Add inverted half pyramid pattern in Java
    Implement a program to print an inverted half pyramid pattern using asterisks.

---
+++ Inverted_half_pyramid.java
@@ -0,0 +1,16 @@
+// printing inverted half pyramid
+
+
+class Pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
