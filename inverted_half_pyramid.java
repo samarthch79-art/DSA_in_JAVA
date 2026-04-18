3e7d25075adf516ac702d9925852db4a9732050a
samarthch79-art <samarthch79@gmail.com>
2026-04-18 18:05:59 +0530

    inverted half pyramid pattern in Java
    Implement inverted half pyramid pattern printing.

---
+++ inverted_half_pyramid.java
@@ -0,0 +1,13 @@
+//printing a inverted half pyramid
+
+
+class Pattern{
+    public static void main(String []args){
+        for(int i=10;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                    System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
