1a7fdeef35a01036ba48a9c88aa002db6e842406
samarthch79-art <samarthch79@gmail.com>
2026-04-18 18:45:55 +0530

    Add Half Pyramid Pattern with Numbers
    Implement a program to print a half pyramid using numbers.

---
+++ Half_pyramid_with_number.java
@@ -0,0 +1,13 @@
+// printing a half pyramid with number
+
+
+class Pattern {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
