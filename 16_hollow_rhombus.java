411d3998336dff00f3b8a28b70e75df0cbd60c63
samarthch79-art <samarthch79@gmail.com>
2026-04-19 16:37:18 +0530

    hollow rhombus pattern printing program
    Implement a program to print a hollow rhombus pattern.

---
+++ 16_hollow_rhombus.java
@@ -0,0 +1,21 @@
+// printing a hollow rhombus
+
+
+class Main{
+    public static void main(String []args){
+        int n=9;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1||i==n||j==1||j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
